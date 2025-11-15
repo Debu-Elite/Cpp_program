2152f5354254e545eb97a9a5e5fa12c119e20e4b
Debu-Elite <gautamdevansh183@gmail.com>
2025-11-15 12:29:42 +0000

    user define type

--- DMA/testDma.cpp
+++ DMA/testDma.cpp
@@ -1,9 +1,30 @@
 #include<iostream>
 using namespace std;
+class Dma
+{
+
+int x,y,z;
+
+public:
+Dma(int a, int b, int c):x(a),y(b),z(c){
+
+cout<<"calling cons"<<endl;
+cout<<"x="<<x<<" y= "<<y<<" z"<<z<<endl;
+}
+
+
+
+
+
+};
 
 int main()
 {
 // array of 10 intgers
+Dma *User=new Dma(12,13,14);
+
+
+
 int *arr=new int[10];
 
 for(int i=0;i<10;i++)
