1362a45bd4a545a2429a5bcb2036d29193af6fb6
Debu-Elite <gautamdevansh183@gmail.com>
2025-11-15 12:40:29 +0000

    const dist calling

--- DMA/testDma.cpp
+++ DMA/testDma.cpp
@@ -4,15 +4,19 @@ class Dma
 {
 
 int x,y,z;
-
+int *ptrs;
 public:
 Dma(int a, int b, int c):x(a),y(b),z(c){
-
-cout<<"calling cons"<<endl;
+ptrs=new int(a+b+c);
+cout<<"calling cons"<<*ptrs<<endl;
 cout<<"x="<<x<<" y= "<<y<<" z"<<z<<endl;
 }
 
-
+~Dma()
+{
+delete ptrs;
+cout<<"dist is called"<<endl;
+}
 
 
 
@@ -22,9 +26,10 @@ int main()
 {
 // array of 10 intgers
 Dma *User=new Dma(12,13,14);
+Dma *User2=new Dma(17,18,19);
 
-
-
+delete User;
+delete User2;
 int *arr=new int[10];
 
 for(int i=0;i<10;i++)
